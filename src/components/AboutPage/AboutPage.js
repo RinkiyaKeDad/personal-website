6af1afb3899403c73c8d8e317ff27a1d3aee2e90
RinkiyaKeDad <arshsharma461@gmail.com>
2020-10-12 19:28:32 +0530

    refactored components

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -4,101 +4,96 @@ import Flippy, { FrontSide, BackSide } from 'react-flippy';
 import dp from '../../static/mainPhoto.jpg';
 import { SocialIcon } from 'react-social-icons';
 
-class App extends React.Component {
-  render() {
-    return (
-      <Flippy
-        flipOnHover={false} // default false
-        flipOnClick={true} // default false
-        flipDirection='horizontal' // horizontal or vertical
-        ref={r => (this.flippy = r)} // to use toggle method like this.flippy.toggle()
-        // if you pass isFlipped prop component will be controlled component.
-        // and other props, which will go to div
+function Aboutpage() {
+  return (
+    <Flippy
+      flipOnHover={false} // default false
+      flipOnClick={true} // default false
+      flipDirection='horizontal' // horizontal or vertical
+      style={{
+        minWidth: '100vw',
+        minHeight: '100vh',
+      }}
+      class='container'
+    >
+      <FrontSide
         style={{
-          minWidth: '100vw',
+          backgroundColor: 'black',
+          color: 'white',
+          display: 'flex',
+          alignItems: 'center',
+          flexDirection: 'column',
+          flexWrap: 'wrap',
+          justifyContent: 'center',
           minHeight: '100vh',
+          minWidth: '100vw',
         }}
-        class='container'
       >
-        <FrontSide
-          style={{
-            backgroundColor: 'black',
-            color: 'white',
-            display: 'flex',
-            alignItems: 'center',
-            flexDirection: 'column',
-            flexWrap: 'wrap',
-            justifyContent: 'center',
-            minHeight: '100vh',
-            minWidth: '100vw',
-          }}
-        >
-          <img src={dp} alt='Avatar' class='avatar' />
-          <div style={{ height: '2rem' }}></div>
-          <h1>About Me</h1>
-          <div style={{ height: '1.5rem' }}></div>
+        <img src={dp} alt='Avatar' class='avatar' />
+        <div style={{ height: '2rem' }}></div>
+        <h1>About Me</h1>
+        <div style={{ height: '1.5rem' }}></div>
 
-          <p>
-            I'm a freelance dev currently pursuing engineering from IIT (BHU),
-            Varanasi. I love all things tech. I believe in writing clean and
-            elegant code while providing a captivating experience to users. I'm
-            also a firm open-source devotee and like contributing to open-source
-            projects. Apart from all this in my free time, I love debating,
-            gaming, or scrolling through IMDb to find the next best thing to
-            watch.
-            <br />
-            <mark>P.S. Tap this page to look at ways to connect.</mark>
-          </p>
-        </FrontSide>
-        <BackSide
-          style={{
-            backgroundColor: 'white',
-            color: 'black',
-            display: 'flex',
-            alignItems: 'center',
-            flexDirection: 'column',
-            flexWrap: 'wrap',
-            justifyContent: 'center',
-            minHeight: '100vh',
-            minWidth: '100vw',
-          }}
-        >
-          <h1>Find Me On</h1>
-          <div class='socials-style'>
-            <div>
-              <SocialIcon
-                url='https://github.com/RinkiyaKeDad'
-                style={{ height: 55, width: 55 }}
-              />
-            </div>
-            <div style={{ width: '.3rem' }}></div>
-            <div>
-              <SocialIcon
-                url='https://www.linkedin.com/in/arsh4/'
-                style={{ height: 55, width: 55 }}
-              />
-            </div>
-            <div style={{ width: '.3rem' }}></div>
-
-            <div>
-              <SocialIcon
-                url='https://codepen.io/RinkiyaKeDad'
-                style={{ height: 55, width: 55 }}
-              />
-            </div>
-            <div style={{ width: '.3rem' }}></div>
-
-            <div>
-              <SocialIcon
-                url='https://www.facebook.com/rinkiya.ke.dad/'
-                style={{ height: 55, width: 55 }}
-              />
-            </div>
+        <p>
+          I'm a freelance dev currently pursuing engineering from IIT (BHU),
+          Varanasi. I love all things tech. I believe in writing clean and
+          elegant code while providing a captivating experience to users. I'm
+          also a firm open-source devotee and like contributing to open-source
+          projects. Apart from all this in my free time, I love debating,
+          gaming, or scrolling through IMDb to find the next best thing to
+          watch.
+          <br />
+          <mark>P.S. Tap this page to look at ways to connect.</mark>
+        </p>
+      </FrontSide>
+      <BackSide
+        style={{
+          backgroundColor: 'white',
+          color: 'black',
+          display: 'flex',
+          alignItems: 'center',
+          flexDirection: 'column',
+          flexWrap: 'wrap',
+          justifyContent: 'center',
+          minHeight: '100vh',
+          minWidth: '100vw',
+        }}
+      >
+        <h1>Find Me On</h1>
+        <div class='socials-style'>
+          <div>
+            <SocialIcon
+              url='https://github.com/RinkiyaKeDad'
+              style={{ height: 55, width: 55 }}
+            />
           </div>
-        </BackSide>
-      </Flippy>
-    );
-  }
+          <div style={{ width: '.3rem' }}></div>
+          <div>
+            <SocialIcon
+              url='https://www.linkedin.com/in/arsh4/'
+              style={{ height: 55, width: 55 }}
+            />
+          </div>
+          <div style={{ width: '.3rem' }}></div>
+
+          <div>
+            <SocialIcon
+              url='https://codepen.io/RinkiyaKeDad'
+              style={{ height: 55, width: 55 }}
+            />
+          </div>
+          <div style={{ width: '.3rem' }}></div>
+
+          <div>
+            <SocialIcon
+              url='https://www.facebook.com/rinkiya.ke.dad/'
+              style={{ height: 55, width: 55 }}
+            />
+          </div>
+        </div>
+      </BackSide>
+    </Flippy>
+  );
 }
 
-export default App;
+export default Aboutpage;
