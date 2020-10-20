c8d4db83bb4561212f32114dd2faedc0e93e82f5
Arsh Sharma <arshsharma461@gmail.com>
2020-10-20 22:15:00 +0530

    new look

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -1,67 +1,36 @@
 import React from 'react';
 import './style.css';
-import Flippy, { FrontSide, BackSide } from 'react-flippy';
 import dp from '../../static/mainPhoto.jpg';
-import { SocialIcon } from 'react-social-icons';
 
 function Aboutpage() {
   return (
-    <Flippy
-      flipOnHover={false} // default false
-      flipOnClick={true} // default false
-      flipDirection='horizontal' // horizontal or vertical
+    <div
       style={{
-        minWidth: '100vw',
+        backgroundColor: 'black',
+        color: 'white',
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
         minHeight: '100vh',
+        minWidth: '100vw',
       }}
-      class='container'
     >
-      <FrontSide
-        style={{
-          backgroundColor: 'black',
-          color: 'white',
-          display: 'flex',
-          alignItems: 'center',
-          flexDirection: 'column',
-          flexWrap: 'wrap',
-          justifyContent: 'center',
-          minHeight: '100vh',
-          minWidth: '100vw',
-        }}
-      >
-        <img src={dp} alt='Avatar' class='avatar' />
-        <div style={{ height: '2rem' }}></div>
-        <h1>About Me</h1>
-        <div style={{ height: '1.5rem' }}></div>
+      <img src={dp} alt='Avatar' class='avatar' />
+      <div style={{ height: '2rem' }}></div>
+      <h1>About Me</h1>
+      <div style={{ height: '1.5rem' }}></div>
 
-        <p>
-          I'm a freelance dev currently pursuing engineering from IIT (BHU),
-          Varanasi. I love all things tech. I believe in writing clean and
-          elegant code while providing a captivating experience to users. I'm
-          also a firm open-source devotee and like contributing to open-source
-          projects. Apart from all this in my free time, I love debating,
-          gaming, or scrolling through IMDb to find the next best thing to
-          watch.
-          <br />
-          <mark>P.S. Tap this page to look at ways to connect.</mark>
-        </p>
-      </FrontSide>
-      <BackSide
-        style={{
-          backgroundColor: 'white',
-          color: 'black',
-          display: 'flex',
-          alignItems: 'center',
-          flexDirection: 'column',
-          flexWrap: 'wrap',
-          justifyContent: 'center',
-          minHeight: '100vh',
-          minWidth: '100vw',
-        }}
-      >
-        <h1>Find Me On</h1>
-      </BackSide>
-    </Flippy>
+      <p>
+        I'm a freelance dev currently pursuing engineering from IIT (BHU),
+        Varanasi. I love all things tech. I believe in writing clean and elegant
+        code while providing a captivating experience to users. I'm also a firm
+        open-source devotee and like contributing to open-source projects. Apart
+        from all this in my free time, I love debating, gaming, or scrolling
+        through IMDb to find the next best thing to watch.
+      </p>
+    </div>
   );
 }
 
